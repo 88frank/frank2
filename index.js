84d0e4f49cc032693a8adf0532d768f40514b71a
88frank <127685178+88frank@users.noreply.github.com>
2023-06-23 23:58:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let numberEl = document.getElementById("number-el")
+let peopleEl = document.getElementById("people-el")
+let count = 0
+
+function increase() {
+    count += 1
+    numberEl.textContent = count 
+}
+
+function decrease() {
+    count --
+    numberEl.textContent = count 
+}
+
+function save() {
+     
+    peopleEl.textContent += "  " +  count + " - "
+    numberEl.textContent = 0
+    count = 0
+}
